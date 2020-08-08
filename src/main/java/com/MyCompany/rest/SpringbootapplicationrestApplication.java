e4f354ee0f76196b26d1b85dec04b4550ddc8aa6
Dipanshu Magoo <dipanshu.magoo96@gmail.com>
2020-08-08 16:32:16 +0530

    First commit

---
+++ src/main/java/com/MyCompany/rest/SpringbootapplicationrestApplication.java
@@ -0,0 +1,13 @@
+package com.MyCompany.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapplicationrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapplicationrestApplication.class, args);
+	}
+
+}
