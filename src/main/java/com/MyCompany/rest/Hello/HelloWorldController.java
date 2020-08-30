9c6e9672b377344d69e32bd9198c89ec17b36907
Dipanshu Magoo <dipanshu.magoo96@gmail.com>
2020-08-30 11:59:45 +0530

    Internationalization added

--- src/main/java/com/MyCompany/rest/Hello/HelloWorldController.java
+++ src/main/java/com/MyCompany/rest/Hello/HelloWorldController.java
@@ -1,6 +1,11 @@
 package com.MyCompany.rest.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloWorldController {
 
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+	
+	
 	
 	// Writing one simple method
 	//@RequestMapping(method=RequestMethod.GET,path="/helloworld")
@@ -24,4 +34,11 @@ public class HelloWorldController {
 	{
 		return new UserDetails("Dipanshu","Magoo","232332");
 	}
+	
+	@GetMapping("/hello-i18n")
+	public String getMessageinternalization(@RequestHeader(name="Accept-Language" , required=false)String locale)
+	{
+  		return messageSource.getMessage("label.hello",null,new Locale(locale));
+	}
+	
 }
--- src/main/java/com/MyCompany/rest/SpringbootapplicationrestApplication.java
+++ src/main/java/com/MyCompany/rest/SpringbootapplicationrestApplication.java
@@ -1,7 +1,16 @@
 package com.MyCompany.rest;
 
+
+
+import java.util.Locale;
+
+import org.hibernate.validator.spi.messageinterpolation.LocaleResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+//import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 // Adding a comment here
 @SpringBootApplication
@@ -9,6 +18,26 @@ public class SpringbootapplicationrestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootapplicationrestApplication.class, args);
+		
+		
+		
+	}
+	
+	@Bean
+	public AcceptHeaderLocaleResolver localeResolver()
+	{
+		AcceptHeaderLocaleResolver localeResolver=new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource()
+	{
+		ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
